998a4f4d62f484bdd05c6032adc07a559172be22
xakus <xakus22@gmail.com>
2021-06-05 19:28:42 +0300

    Add SQL files

---
+++ src/main/resources/SQL/family.sql
@@ -0,0 +1,3 @@
+INSERT INTO "family"("id", "create_date", "name") VALUES (1, '2021-06-03 04:08:20', 'Super Femaly');
+INSERT INTO "family"("id", "create_date", "name") VALUES (2, '2021-06-03 04:09:49', 'My beloved family');
+INSERT INTO "family"("id", "create_date", "name") VALUES (3, '2021-06-03 04:11:07', 'Dream family');
